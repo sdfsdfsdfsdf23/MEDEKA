9cdd9225c3b51e1aa799f07abc59c453390daa2f
Maxim Bazheev <akiokazaki426@gmail.com>
2022-10-08 20:25:47 +0300

    Initial commit

--- lw0/Mlw0.php
+++ lw0/Mlw0.php
@@ -1,6 +1,12 @@
 <?php
-    $a = "2";
 
-    echo $a;
-?>
-/*asd*/
+function calculator($calc)
+{
+    $result = eval("return $calc;");
+    if (is_string($result)) {
+        echo "ERORCHIK";
+    } else {
+        echo $result;
+    }
+}
+calculator('1+2*3-4/5'); //answer 6.2
