984c92eb18ac59b2fb8e4cb3e7ac503a9e61c913
Maxim Bazheev <akiokazaki426@gmail.com>
2022-10-09 16:43:52 +0300

    Починил

--- lw0/Mlw0.php
+++ lw0/Mlw0.php
@@ -1,12 +1,17 @@
 <?php
 
-function calculator($calc)
+function calculator(string $primer)
 {
-    $result = eval("return $calc;");
-    if (is_string($result)) {
-        echo "ERORCHIK";
-    } else {
-        echo $result;
+    $arrayznak = array("?", ")");
+
+    foreach ($arrayznak as $znak) {
+        $pos = strpos($primer, $znak);
+        if ($pos === false) {
+            $result = eval("return (float)$primer;");
+        } else {
+            echo "Error";
+        }
     }
+    echo $result;
 }
 calculator('1+2*3-4/5'); //answer 6.2
