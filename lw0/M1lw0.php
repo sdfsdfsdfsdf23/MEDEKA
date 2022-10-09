548ecefd52fad0cad5315c773d5f7d2642c6ce76
Maxim Bazheev <akiokazaki426@gmail.com>
2022-10-09 16:43:14 +0300

    поправил замечания

--- lw0/M1lw0.php
+++ lw0/M1lw0.php
@@ -3,25 +3,25 @@
 $arrayTimeFirst = [];
 $arrayTimeSecond = [];
 
-function sumTime($timeFirst, $timeSecond)
+function sumTime(string $timeFirst, string $timeSecond)
 {
     $arrayTimeFirst = explode(':', $timeFirst);
     $arrayTimeSecond = explode(':', $timeSecond);
     $timeArrayThird0 = $arrayTimeFirst[0] + $arrayTimeSecond[0];
     $timeArrayThird1 = $arrayTimeFirst[1] + $arrayTimeSecond[1];
     $timeArrayThird2 = $arrayTimeFirst[2] + $arrayTimeSecond[2];
-    if ($timeArrayThird2 >= 60) {
+    while ($timeArrayThird2 >= 60) {
         $timeArrayThird1 = $timeArrayThird1 + 1;
         $timeArrayThird2 = $timeArrayThird2 - 60;
     }
-    if ($timeArrayThird1 >= 60) {
+    while ($timeArrayThird1 >= 60) {
         $timeArrayThird0 = $timeArrayThird0 + 1;
         $timeArrayThird1 = $timeArrayThird1 - 60;
     }
 
-    if ($timeArrayThird0 >= 24) {
+    while ($timeArrayThird0 >= 24) {
         $timeArrayThird0 = $timeArrayThird0 - 24;
     }
-    echo $timeArrayThird0 . ":", $timeArrayThird1 . ":", $timeArrayThird2;
+    echo (int)$timeArrayThird0 . ":", (int)$timeArrayThird1 . ":", (int)$timeArrayThird2;
 }
-sumTime('10:30:90', '10:60:12');
+sumTime('40:30:90', '10:60:12');
