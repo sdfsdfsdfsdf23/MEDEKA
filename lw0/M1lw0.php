f4a91f45ac3edda43853f38a3032376f37090c08
Maxim Bazheev <akiokazaki426@gmail.com>
2022-10-11 05:58:23 +0300

    fix(lw0): добавил возвращаемый тип функции

--- lw0/M1lw0.php
+++ lw0/M1lw0.php
@@ -2,26 +2,28 @@
 
 $arrayTimeFirst = [];
 $arrayTimeSecond = [];
+$timeArrayThird = [];
 
-function sumTime(string $timeFirst, string $timeSecond)
+function sumTime(string $timeFirst, string $timeSecond): string
 {
     $arrayTimeFirst = explode(':', $timeFirst);
     $arrayTimeSecond = explode(':', $timeSecond);
-    $timeArrayThird0 = $arrayTimeFirst[0] + $arrayTimeSecond[0];
-    $timeArrayThird1 = $arrayTimeFirst[1] + $arrayTimeSecond[1];
-    $timeArrayThird2 = $arrayTimeFirst[2] + $arrayTimeSecond[2];
-    while ($timeArrayThird2 >= 60) {
-        $timeArrayThird1 = $timeArrayThird1 + 1;
-        $timeArrayThird2 = $timeArrayThird2 - 60;
+    $timeArrayThird[0] = $arrayTimeFirst[0] + $arrayTimeSecond[0];
+    $timeArrayThird[1] = $arrayTimeFirst[1] + $arrayTimeSecond[1];
+    $timeArrayThird[2] = $arrayTimeFirst[2] + $arrayTimeSecond[2];
+    while ($timeArrayThird[2] > 59) {
+
+        $timeArrayThird[1] = $timeArrayThird[1] + 1;
+        $timeArrayThird[2] = $timeArrayThird[2] - 60;
     }
-    while ($timeArrayThird1 >= 60) {
-        $timeArrayThird0 = $timeArrayThird0 + 1;
-        $timeArrayThird1 = $timeArrayThird1 - 60;
+    while ($timeArrayThird[1] > 59) {
+        $timeArrayThird[0] = $timeArrayThird[0] + 1;
+        $timeArrayThird[1] = $timeArrayThird[1] - 60;
     }
 
-    while ($timeArrayThird0 >= 24) {
-        $timeArrayThird0 = $timeArrayThird0 - 24;
+    while ($timeArrayThird[0] > 23) {
+        $timeArrayThird[0] = $timeArrayThird[0] - 24;
     }
-    echo (int)$timeArrayThird0 . ":", (int)$timeArrayThird1 . ":", (int)$timeArrayThird2;
+    return $timeArrayThird[0].':'.$timeArrayThird[1].':'.$timeArrayThird[2];
 }
-sumTime('40:30:90', '10:60:12');
+echo SumTime('40:90:60', '10:60:30');
