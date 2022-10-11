350ac2e415334256d67c2bafe9fedcab836524dc
Maxim Bazheev <akiokazaki426@gmail.com>
2022-10-11 16:31:40 +0300

    feat(lw0): обновил функционал

--- lw0/M1lw0.php
+++ lw0/M1lw0.php
@@ -6,6 +6,27 @@ $timeArrayThird = [];
 
 function sumTime(string $timeFirst, string $timeSecond): string
 {
+    $arrayZnaki = ['!', '@', '#', '%', '^', '&', '?', '-', '_'];
+
+    $arrFirst = str_split($timeFirst);
+    $arrSecond = str_split($timeSecond);
+    $arrLength = count(array_merge($arrFirst, $arrSecond));
+    for ($i = 0; $i < $arrLength; $i++) {
+        while (ctype_alpha($arrFirst[$i])) {
+            unset($arrFirst[$i]);
+        }
+        while (ctype_alpha($arrSecond[$i])) {
+            unset($arrSecond[$i]);
+        }
+        if (in_array($arrFirst[$i], $arrayZnaki)) {
+            unset($arrFirst[$i]);
+        }
+        if (in_array($arrSecond[$i], $arrayZnaki)) {
+            unset($arrSecond[$i]);
+        }
+    }
+    $timeFirst = implode($arrFirst);
+    $timeSecond = implode($arrSecond);
     $arrayTimeFirst = explode(':', $timeFirst);
     $arrayTimeSecond = explode(':', $timeSecond);
     $timeArrayThird[0] = $arrayTimeFirst[0] + $arrayTimeSecond[0];
@@ -24,6 +45,6 @@ function sumTime(string $timeFirst, string $timeSecond): string
     while ($timeArrayThird[0] > 23) {
         $timeArrayThird[0] = $timeArrayThird[0] - 24;
     }
-    return $timeArrayThird[0].':'.$timeArrayThird[1].':'.$timeArrayThird[2];
+    return $timeArrayThird[0] . ':' . $timeArrayThird[1] . ':' . $timeArrayThird[2];
 }
-echo SumTime('40:90:60', '10:60:30');
+echo SumTime('40?asd?:9?0:as6@#0', '10:asd6!0a#sd:3?0ddasd'); //4:31:30
