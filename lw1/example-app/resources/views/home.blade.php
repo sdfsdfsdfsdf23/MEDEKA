589b72af448962d88b1d06a07fc502f1362d928c
sdfsdfsdfsdf23 <akiokazaki426@gmail.com>
2022-10-27 19:36:24 +0300

    Initial commit

---
+++ lw1/example-app/resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $name }}</title>
+</head>
+
+<body>
+    <div class="maincontainer">
+        <div class="img">
+            <img src="{{ asset('ifDFxVeptvY.jpg') }}" alt="">
+        </div>
+        <div class="container">
+            <div class="item">
+                <h4>Крипочек</h4>
+                Звать Андрюха
+            </div>
+            <div class="item">
+                <h4>Крипочек</h4>
+                Звать Андрюха
+            </div>
+            <div class="item">
+                <h4>Крипочек</h4>
+                Звать Андрюха
+            </div>
+            <div class="item">
+                <h4>Крипочек</h4>
+                Звать Андрюха
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
