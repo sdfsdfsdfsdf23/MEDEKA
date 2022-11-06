a38a224e9c03eab93780c46e020c571b8c2c1822
sdfsdfsdfsdf23 <akiokazaki426@gmail.com>
2022-11-06 22:15:05 +0300

    feat(lw2):функционал по лекции5

--- lw1/routes/web.php
+++ lw1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', [HomeController::class, 'index',]);
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
+Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::get('articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+Route::put('articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
+Route::delete('article/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+Route::get('articles/{article}/show', [ArticleController::class, 'show'])->name('articles.show');
